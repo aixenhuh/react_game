6844438679d1a038f592f77e15f557825a72f371
aixenhuh@gmail.com <aixenhuh@gmail.com>
2019-11-06 18:15:27 +0900

    보드 커밋

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# react_game
+react_game
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Game from './Components/Game';
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Game></Game>
+      </div>
+    )
+  }
+}
+
+export default App;
